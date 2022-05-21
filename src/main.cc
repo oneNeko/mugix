f8d534ef0982c549710339c03595c65e3c6e79ce
oneNeko <oneneko@qq.com>
2022-05-22 01:42:55 +0800

    以后台形式运行

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,21 @@
+#include <unistd.h>
+
 #include "server/core/socket_controler.h"
 #include "server/core/logger.h"
 
 int main(int argc, char *argv[])
 {
 	Logger::getLogger().setLogLevel(Logger::LALL);
+	Logger::getLogger().setFileName("mugix.log");
+
+	info("mugix start ...");
+
+	daemon(1, 1);
+
 	mugix::server::SockerControler sc;
 	sc.run();
+
+	error("mugix 已退出");
 
 	return 0;
 }
